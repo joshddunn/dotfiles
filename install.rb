2a607f9382c7d773952a876957f865f75c7d5928
joshddunn <joshdunn.uwo@gmail.com>
2021-04-07 01:43:04 -0400

    add finish message and fix on my zsh install

--- install.rb
+++ install.rb
@@ -1,4 +1,33 @@
+def dir_exists?(dir)
+  if File.exist?(File.expand_path(dir))
+    p "#{dir} already exists"
+    true
+  else
+    false
+  end
+end
+
 system("brew bundle")
+
+unless dir_exists?("~/.oh-my-zsh")
+  system("sh -c \"$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)\" \" \" --unattended --keep-zshrc")
+end
+
+unless dir_exists?("~/.oh-my-zsh/custom/plugins/zsh-autosuggestions")
+  system("git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions")
+end
+
+unless dir_exists?("~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" )
+  system("git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting")
+end
+
+unless dir_exists?("~/.oh-my-zsh/custom/plugins/zsh-completions")
+  system("git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions")
+end
+
+unless dir_exists?("~/.oh-my-zsh/custom/plugins/z.lua")
+  system("git clone https://github.com/skywind3000/z.lua.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/z.lua")
+end
 
 system("grep -q -F 'source ~/dotfiles/zshrc' ~/.zshrc || echo 'source ~/dotfiles/zshrc' >> ~/.zshrc")
 system("grep -q -F 'source ~/dotfiles/zshrc.work' ~/.zshrc || echo 'source ~/dotfiles/zshrc.work' >> ~/.zshrc")
@@ -17,15 +46,6 @@ files = {
   "~/dotfiles/lua" => "~/.config/nvim/lua",
   "~/dotfiles/sshrc.d" => "~/.sshrc.d",
 }
-
-def dir_exists?(dir)
-  if File.exist?(File.expand_path(dir))
-    p "#{dir} already exists"
-    true
-  else
-    false
-  end
-end
 
 files.each do |source, destination|
   system("ln -s #{source} #{destination}") unless dir_exists?(destination)
@@ -55,22 +75,4 @@ system("pip install --user pynvim")
 system("pip3 install --user pynvim")
 system("gem install -u neovim")
 
-unless dir_exists?("~/.oh-my-zsh/custom/plugins/zsh-autosuggestions")
-  system("git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions")
-end
-
-unless dir_exists?("~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" )
-  system("git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting")
-end
-
-unless dir_exists?("~/.oh-my-zsh/custom/plugins/zsh-completions")
-  system("git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions")
-end
-
-unless dir_exists?("~/.oh-my-zsh/custom/plugins/z.lua")
-  system("git clone https://github.com/skywind3000/z.lua.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/z.lua")
-end
-
-unless dir_exists?("~/.oh-my-zsh")
-  system("sh -c \"$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)\"")
-end
+puts "All done! Go ahead and restart your terminal."
