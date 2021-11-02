1dac8412c4fe7953af803f447b79f3a2b19ae9df
joshddunn <joshdunn.uwo@gmail.com>
2021-11-02 00:41:48 -0400

    update configuration for glow

--- install/fragments/dotfiles.rb
+++ install/fragments/dotfiles.rb
@@ -15,7 +15,8 @@ class Dotfiles
       '~/dotfiles/asdf/tool-versions' => '~/.tool-versions',
       '~/dotfiles/nvim/init.lua' => '~/.config/nvim/init.lua',
       '~/dotfiles/nvim/lua' => '~/.config/nvim/lua',
-      '~/dotfiles/tmuxinator' => '~/.config/tmuxinator'
+      '~/dotfiles/tmuxinator' => '~/.config/tmuxinator',
+      '~/dotfiles/glow.yml' => '~/Library/Preferences/glow/glow.yml'
     }.each { |source, destination| Utils.symlink(source, destination) }
   end
 end
