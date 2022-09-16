4311ef0948d81485fda24b6d3e3effed70362137
joshddunn <joshdunn.uwo@gmail.com>
2022-09-16 01:53:21 -0400

    add pack for bootstrap packer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,19 +1,4 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
-return require("packer").startup(function(use)
-  use 'wbthomason/packer.nvim'
-
+require("pack")(function(use)
   use "airblade/vim-gitgutter"
   use "ap/vim-css-color"
   use "arecarn/vim-fold-cycle"
@@ -52,8 +37,4 @@ return require("packer").startup(function(use)
 
   use { "neoclide/coc.nvim", branch = "release" }
   use { "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" }
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end)
