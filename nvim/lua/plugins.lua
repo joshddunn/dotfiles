d4eb86800efb83cb6bd1340c6a639ccd49591047
joshddunn <joshdunn.uwo@gmail.com>
2022-09-16 01:40:20 -0400

    update packer install

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,51 +1,59 @@
-local fn = vim.fn
-local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
-  vim.cmd "packadd packer.nvim"
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
-vim.cmd "autocmd BufWritePost plugins.lua PackerCompile"
+local packer_bootstrap = ensure_packer()
 
-return require("packer").startup(
-  function()
-    use "airblade/vim-gitgutter"
-    use "ap/vim-css-color"
-    use "arecarn/vim-fold-cycle"
-    use "fnune/base16-vim"
-    use "edkolev/tmuxline.vim"
-    use "elixir-editors/vim-elixir"
-    use "jlanzarotta/bufexplorer"
-    use "joom/vim-commentary"
-    use "joshddunn/vim-glob"
-    use "jparise/vim-graphql"
-    use "junegunn/fzf"
-    use "junegunn/fzf.vim"
-    use "junegunn/vim-easy-align"
-    use "justinmk/vim-sneak"
-    use "leafgarland/typescript-vim"
-    use "maxbrunsfeld/vim-yankstack"
-    use "maxmellon/vim-jsx-pretty"
-    use "moll/vim-node"
-    use "nathanaelkane/vim-indent-guides"
-    use "pangloss/vim-javascript"
-    use "pseewald/vim-anyfold"
-    use "ruanyl/vim-gh-line"
-    use "sudar/vim-arduino-syntax"
-    use "tpope/vim-abolish"
-    use "tpope/vim-endwise"
-    use "tpope/vim-fugitive"
-    use "tpope/vim-rails"
-    use "tpope/vim-repeat"
-    use "tpope/vim-surround"
-    use "vim-airline/vim-airline" -- slow
-    use "vim-airline/vim-airline-themes"
-    use "w0rp/ale"
-    use "ellisonleao/glow.nvim"
-    use "noahtheduke/vim-just"
-    use "sheerun/vim-polyglot"
+return require("packer").startup(function(use)
+  use 'wbthomason/packer.nvim'
 
-    use { "neoclide/coc.nvim", branch = "release" }
-    use { "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" }
+  use "airblade/vim-gitgutter"
+  use "ap/vim-css-color"
+  use "arecarn/vim-fold-cycle"
+  use "fnune/base16-vim"
+  use "edkolev/tmuxline.vim"
+  use "elixir-editors/vim-elixir"
+  use "jlanzarotta/bufexplorer"
+  use "joom/vim-commentary"
+  use "joshddunn/vim-glob"
+  use "jparise/vim-graphql"
+  use "junegunn/fzf"
+  use "junegunn/fzf.vim"
+  use "junegunn/vim-easy-align"
+  use "justinmk/vim-sneak"
+  use "leafgarland/typescript-vim"
+  use "maxbrunsfeld/vim-yankstack"
+  use "maxmellon/vim-jsx-pretty"
+  use "moll/vim-node"
+  use "nathanaelkane/vim-indent-guides"
+  use "pangloss/vim-javascript"
+  use "pseewald/vim-anyfold"
+  use "ruanyl/vim-gh-line"
+  use "sudar/vim-arduino-syntax"
+  use "tpope/vim-abolish"
+  use "tpope/vim-endwise"
+  use "tpope/vim-fugitive"
+  use "tpope/vim-rails"
+  use "tpope/vim-repeat"
+  use "tpope/vim-surround"
+  use "vim-airline/vim-airline" -- slow
+  use "vim-airline/vim-airline-themes"
+  use "w0rp/ale"
+  use "ellisonleao/glow.nvim"
+  use "noahtheduke/vim-just"
+  use "sheerun/vim-polyglot"
+
+  use { "neoclide/coc.nvim", branch = "release" }
+  use { "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" }
+
+  if packer_bootstrap then
+    require('packer').sync()
   end
-)
+end)
