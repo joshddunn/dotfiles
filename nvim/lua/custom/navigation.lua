b8e31413462f2a93b071a2000a5d1d5125c1f2b7
joshddunn <joshdunn.uwo@gmail.com>
2023-07-05 02:56:35 -0400

    update navigation

--- nvim/lua/custom/navigation.lua
+++ nvim/lua/custom/navigation.lua
@@ -5,6 +5,12 @@ vim.g.vim_navigation = {
   phoenix = {
     migrations = {
       dir = "priv/repo/migrations/"
+    },
+    alternate = {
+      dir = "lib/",
+      file_pattern = ".ex",
+      alt_dir = "test/",
+      alt_file_pattern = "_test.exs"
     }
   }
 }
@@ -13,6 +19,7 @@ if vim.g.vim_navigation then
   local framework = "phoenix"
   local config = vim.g.vim_navigation[framework]
 
+  -- migrations
   function NavigationEditMostRecentMigrationFile()
     local filenames = vim.fn.split(vim.fn.glob(config.migrations.dir .. "*"), "\n")
     vim.api.nvim_command("edit " .. filenames[#filenames])
@@ -20,5 +27,37 @@ if vim.g.vim_navigation then
 
   if config.migrations then
     vim.api.nvim_create_user_command("Emigration", NavigationEditMostRecentMigrationFile, { nargs = 0 })
+  end
+
+  -- alternate
+  function NavigationEditAlternateFile()
+    local alt_config = config.alternate
+    local filename = lib.replace(vim.api.nvim_buf_get_name(0), vim.fn.getcwd() .. '/', '')
+    local old_dir = ''
+    local new_dir = ''
+    local old_pattern = ''
+    local new_pattern = ''
+
+    if (filename:match(alt_config.file_pattern .. '$')) then
+      old_dir = alt_config.dir
+      new_dir = alt_config.alt_dir
+      old_pattern = alt_config.file_pattern
+      new_pattern = alt_config.alt_file_pattern
+    elseif (filename:match(alt_config.alt_file_pattern .. '$')) then
+      old_dir = alt_config.alt_dir
+      new_dir = alt_config.dir
+      old_pattern = alt_config.alt_file_pattern
+      new_pattern = alt_config.file_pattern
+    else
+      vim.api.nvim_echo({{"No alternate file defined"}}, false, {})
+      return
+    end
+
+    local alternate_filename = lib.replace(filename, old_pattern, new_pattern)
+    vim.api.nvim_command("edit " .. lib.replace(alternate_filename, old_dir, new_dir))
+  end
+
+  if config.alternate then
+    vim.api.nvim_create_user_command("A", NavigationEditAlternateFile, { nargs = 0 })
   end
 end
