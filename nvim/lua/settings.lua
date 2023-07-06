504322d77525d8f9a4b0dcfc83832e06ef0e362a
joshddunn <joshdunn.uwo@gmail.com>
2023-07-06 17:21:11 -0400

    update global replace

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -119,12 +119,14 @@ vim.api.nvim_set_keymap("v", "<leader>G", "y:SearchSelection <C-r>0<cr>", { nore
 
 -- global replace
 function GlobalReplace(opts)
+  local tmpfile = "global-replace-tmp.txt"
   local old = opts.fargs[1]
   local new = opts.fargs[2]
-  local filenames = lib.split(vim.fn.system({ "rg", "-l", old }), "\n")
+  local filenames = lib.split(vim.fn.trim(vim.fn.system({ "rg", "-l", old })), "\n")
   lib.each(filenames, function(filename)
     local text = vim.fn.system({ "rg", "--passthru", old, "-r", new, filename })
-    vim.fn.system({ "sponge", filename }, text)
+    vim.fn.system({ "tee", tmpfile }, text)
+    vim.fn.system({ "mv", tmpfile, filename })
   end)
   vim.api.nvim_echo({{"All Done."}}, false, {})
 end
