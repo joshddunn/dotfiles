2e91ceacd741cec36d9907192f067d2cf7c35007
Josh Dunn <joshdunn.uwo@gmail.com>
2023-09-02 23:45:55 -0400

    use buffline

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        style_preset = bufferline.style_preset.minimal
+      }
+    })
+  end
+}
--- nvim/lua/plugins/vim-airline.lua
+++ nvim/lua/plugins/vim-airline.lua
@@ -2,7 +2,6 @@ return {
   "vim-airline/vim-airline",
   dependencies = "vim-airline/vim-airline-themes",
   init = function()
-    vim.g["airline#extensions#tabline#enabled"] = 1
     vim.g.airline_theme = "base16_tomorrow"
   end
 }
