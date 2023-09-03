8b7641e86983f0c26e2ba5ff32fb71fb1360172a
Josh Dunn <joshdunn.uwo@gmail.com>
2023-09-03 00:33:59 -0400

    don't show close icon

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -5,7 +5,8 @@ return {
     local bufferline = require("bufferline")
     bufferline.setup({
       options = {
-        style_preset = bufferline.style_preset.minimal
+        style_preset = bufferline.style_preset.minimal,
+        show_buffer_close_icons = false
       }
     })
   end
