b47b10d2bae9c644c16fe339955e7f4ac0c1893e
Josh Dunn <joshdunn.uwo@gmail.com>
2023-09-05 20:48:37 -0400

    update characters

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -37,7 +37,7 @@ return {
     require("lualine").setup({
       options = {
         theme = tomorrow_night,
-        component_separators = { left = ">", right = "<"},
+        component_separators = { left = "❯", right = "❮"},
         section_separators = { left = "", right = ""},
       },
       sections = {
