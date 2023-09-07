7e83dc38800c65c21c2642565802ac7ca205617d
Josh Dunn <joshdunn.uwo@gmail.com>
2023-09-07 18:42:12 -0400

    get rid of yankstack

--- nvim/lua/plugins/vim-yankstack.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "maxbrunsfeld/vim-yankstack",
-  init = function()
-    vim.keymap.set("n", "c-p", "<Plug>yankstack_substitute_older_paste")
-    vim.keymap.set("n", "c-P", "<Plug>yankstack_substitute_newer_paste")
-  end
-}
