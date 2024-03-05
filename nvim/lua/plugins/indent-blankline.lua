75032e97392bd9acfdfa9bfce87acb17146d72a1
joshddunn <joshdunn.uwo@gmail.com>
2024-03-05 10:45:20 -0500

    update ibl and nerdtree

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,16 +2,25 @@ local colors = require("colors")
 
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   config = function()
-    vim.api.nvim_set_hl(0, "IndentBlanklineIndent", {
-      fg = colors.gray5,
-      nocombine = true
-    })
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "IndentBlanklineIndent", { fg = colors.gray5 })
+    end)
 
-    require("indent_blankline").setup {
-      char_highlight_list = { "IndentBlanklineIndent" },
-      show_trailing_blankline_indent = false,
-      show_current_context = true,
-    }
+    require("ibl").setup({
+      indent = {
+        highlight = "IndentBlanklineIndent",
+        smart_indent_cap = true,
+      },
+      whitespace = {
+        remove_blankline_trail = true
+      },
+      scope = {
+        show_start = false,
+        show_end = false,
+      }
+    })
   end
 }
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "tsx", "elixir", "heex", "yaml", "terraform", "astro", "css" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "tsx", "elixir", "heex", "yaml", "terraform", "astro", "css", "javascript" },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
