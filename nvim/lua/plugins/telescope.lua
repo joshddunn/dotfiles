4ae3ab38dffbc62693b5aa524ed6db9f679ab1a3
Josh Dunn <joshdunn.uwo@gmail.com>
2024-06-04 22:42:41 -0400

    update telescope version

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ lib = require("lib")
 
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.2",
+  branch = "0.1.x",
   dependencies = "nvim-lua/plenary.nvim",
   config = function()
     local glob_ignore = {
