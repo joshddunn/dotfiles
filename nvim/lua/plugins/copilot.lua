e26e232c61f4e953973904296448013e102ee24c
Josh Dunn <joshdunn.uwo@gmail.com>
2025-03-14 23:26:33 -0400

    update comment

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   "github/copilot.vim",
   init = function()
-    -- if i use other keymap api, it inserts weird characters
+    -- other keymap api inserts weird characters
     vim.api.nvim_set_keymap("i", "<C-j>", "copilot#Accept(\"\\<CR>\")",
       { noremap = true, silent = true, expr = true, script = true })
     vim.g.copilot_no_tab_map = true
